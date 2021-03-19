712bba1699224973faabe3b9fb8032020568f863
Ricardo-Patino <rickpatinor@gmail.com>
2021-03-19 03:48:53 +0000

    flux update

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -3,9 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
 	return (
-		<nav className="navbar navbar-light bg-light mb-3 bg-dark">
+		<nav className="navbar navbar-dark bg-dark mb-3 bg-dark">
 			<Link to="/">
-				<span className="navbar-brand text-light mb-0 h1">React Boilerplate</span>
+				<span className="navbar-brand text-light mb-0 h1">
+					<img
+						width={80}
+						height={40}
+						src="https://tse3.mm.bing.net/th?id=OIP.IyVIK3Cw9fZ4iHMlp2FZmAHaD5&pid=Api&P=0&w=295&h=156"
+						alt="Planets"
+					/>
+				</span>
 			</Link>
 			<div className="ml-auto">
 				<Link to="/demo">
--- src/js/views/character.js
+++ src/js/views/character.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 
-export const Character = () => (
-	<div className="text-center mt-5">
-		<h1>Characters</h1>
-		<Link to="/">
-			<botton className="btn btn-primary">Ir a Home</botton>
-		</Link>
-	</div>
-);
+export const Character = () => {
+	const { store } = useContext(Context);
+	console.log("data", store.peoples);
+
+	return (
+		<div className="text-center mt-5">
+			<h1>Characters</h1>
+			<Link to="/">
+				<botton className="btn btn-primary">Ir a Home</botton>
+			</Link>
+		</div>
+	);
+};
